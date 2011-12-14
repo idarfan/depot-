a1c5321ccc3487a3cb6cb49e20ca0cce9de5e41e
idarfan <mr.idarfan@gmail.com>
2011-12-14 16:15:43 +0800

    level one cart

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '639ed6356b0a2f3c23be86666b72df42521a4cc24e32be03c1501592b5903e5f9c59cbb619d85aead151f6d0fc0fdf30066273eaa3a80579adaaba4fe80521f0'
